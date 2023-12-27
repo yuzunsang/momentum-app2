f657220e5d0c68060de0ff4401168ba22a0e78c2
tim1007 <111577841+yuzunsang@users.noreply.github.com>
2023-12-27 16:29:39 +0900

    #4.7 까지

--- app.js
+++ app.js
@@ -1,19 +1,43 @@
 const loginForm = document.querySelector(".login-form");
-const loginInput = loginForm.querySelector("input");
-const link = document.querySelector("a");
+const logoutForm = document.querySelector(".logout-form");
+const loginInput = loginForm.querySelector(".login-input");
+const logoutInput = logoutForm.querySelector(".logout-input");
+const loginTypeds = loginForm.querySelector(".login-typeds");
+const greeting = document.querySelector(".greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+const paintGreetings = (username) => {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+  logoutForm.classList.remove(HIDDEN_CLASSNAME);
+};
 
 const onLoginSubmit = (event) => {
   event.preventDefault();
-  const username = loginInput.value;
+  const username = loginTypeds.value;
 
-  if (username === "") {
-    console.log("Please write your name.");
-  } else console.log("Hello " + username);
+  localStorage.setItem(USERNAME_KEY, username);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  paintGreetings(username);
 };
 
-const onLinkClick = () => {
-  alert("clicked");
+const onLogoutSubmit = (event) => {
+  localStorage.removeItem(USERNAME_KEY);
+  window.location.reload();
 };
 
-loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", onLinkClick);
+if (!savedUsername) {
+  // show the form
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  // show the greetings
+  paintGreetings(savedUsername);
+  logoutInput.addEventListener("submit", onLogoutSubmit);
+}
+
+logoutForm.addEventListener("submit", onLogoutSubmit);
