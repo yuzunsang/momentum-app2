b3550d7ef259b8fde0ee22e639ad0672c54fd20e
tim1007 <111577841+yuzunsang@users.noreply.github.com>
2023-12-29 14:59:09 +0900

    quotes and backgrounds done

---
+++ js/background.js
@@ -0,0 +1,11 @@
+const images = [
+  "photo-1542442828-287217bfb87f.avif",
+  "photo-1490077476659-095159692ab5.avif",
+  "premium_photo-1678233319969-63662b4244f1.avif",
+];
+
+const pickImage = images[~~(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+bgImage.src = `img/${pickImage}`;
+document.body.appendChild(bgImage);
